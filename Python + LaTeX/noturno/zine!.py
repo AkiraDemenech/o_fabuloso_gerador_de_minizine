993244895f6b41d7c6726f417cc2936e3f6a163e
AkiraDemenech <guiakira.demenechmori@gmail.com>
2023-10-25 20:11:39 -0300

    Caju é ela/dela

--- Python + LaTeX/noturno/zine!.py
+++ Python + LaTeX/noturno/zine!.py
@@ -265,7 +265,7 @@ for amigos in [
 		['Pedro Miguel Castelano de Oubrirres e Punha',	'de Pedro Miguel Castelano de Oubrirres e Punha',	'um pedaço de bolo',	'fabioalcover.jpg',	('Fábio Alcover','\\texttt{@fabioalcover}')],
 		['Pitu',	'de Pitu',	'um jogo de chaves de fenda',	'fabioalcover2.jpg',	('Fábio Alcover','\\texttt{@fabioalcover}')],
 		['a mãe do Pitu',	'da mãe do Pitu',	'uma roupa bem linda',	'fabioalcover3.jpg',	('Fábio Alcover','\\texttt{@fabioalcover}')],		
-		['Caju',	'de Caju',	'um anel da sorte (que na verdade dá azar)',	'Sapinho.png',	('Luana Bibiano','\\texttt{@luana.bibiano.m}')]		
+		['Caju',	'da Caju',	'um anel da sorte (que na verdade dá azar)',	'Sapinho.png',	('Luana Bibiano de Melo','\\texttt{@luana.bibiano.m}')]		
 	]
 ]: 
 	permutacoes = []
