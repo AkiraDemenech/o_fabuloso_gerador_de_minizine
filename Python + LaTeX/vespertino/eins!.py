1c36390907725dac2720b23f02b7870f97522867
Guilherme Akira Mori <akira.demenech@gmail.com>
2024-03-08 04:11:57 -0300

    Linhas vazias

--- Python + LaTeX/vespertino/eins!.py
+++ Python + LaTeX/vespertino/eins!.py
@@ -2,47 +2,17 @@ import threading
 import random	
 import os	
 
-
-
-
-	
-		
-	
-
-
-
-
-	
-	
-
-
-
-
-
-
-
-
-
 def escolher (partes, x, y = None):
 	
 
 from eins import dados 
 
-		
-	
-	
-	
-		
-			
-		
 dir = 'à direita'
 esq = 'à esquerda'
 
-		
 coluna = 'lsp'
 largura = 4
 
-	
 teste = [list(coluna) for c in range(largura)]
 
 pos = []
@@ -50,12 +20,6 @@ for c in range(len(teste)):
 	for h in range(len(teste[c])):
 		pos.insert(random.randint(0,len(pos)),(c,h))
 
-	
-		
-				
-		
-	
-	
 while len(pos):	
 	ac,ah = pos.pop()
 	a = teste[ac][ah]
@@ -63,16 +27,4 @@ while len(pos):
 	bc,bh = pos.pop()	
 	b = teste[bc][bh]
 
-			
-					
-				
-		
-	
-	
-		
-		
-		
-
-		
-			
 	print(ac - bc, '\t', a, ac,ah, '\t', b, bc,bh)
